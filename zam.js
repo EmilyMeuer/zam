c47b6068e70b8e09e8705e99ab4e8642a6bef31d
Brandon Barber <roecrew@gmail.com>
2018-01-04 02:10:46 -0600

    6.0

--- zam.js
+++ zam.js
@@ -1,69 +1,113 @@
-export default function Zam() {}
-
-Zam.idCounter = 0;
-
-Zam.getId = function() {
-	return Zam.idCounter++;
-}
-
-Zam.component = function() {
-	this.id = 'a' + Zam.getId();
-	this.children = {};
-	this.body = function(html) {
-		var elem = document.createElement('div');
-		elem.id = this.id;
-		elem.innerHTML = html;
-		this.anchor = elem;
-		this.html = elem.outerHTML;
+export default class Zam {
+	constructor(html) {
+		this.children = {};
+		this.html = html;
+		this.e = document.createElement('div');
+		this.e.innerHTML = html
+		this.node = this.e.children[0];
 		if(this.constructor.name === 'Root') {
-			document.querySelectorAll('body')[0].innerHTML = this.html;
-			this.mount();
+			document.querySelectorAll('body')[0].appendChild(this.e);
 		}
 	}
-	this.cache = function(component, key) {
-		if(key) {
-			component.parent = this;
-			this.children[key] = component;
-			return component;
-		} else {
-			return this.children[component];
-		}
+
+	append(component, key) {
+		component.parent = this;
+		this.children[key] = component;
+		this.e.children[0].appendChild(component.e);
 	}
-	this.mount = function() {
-		this.anchor = document.getElementById(this.id);
-		this.html = this.anchor.outerHTML;
-		for (var key in this.children) {
-			if(document.getElementById(this.children[key].id) !== null) {
-				this.children[key].mount();
-			} else {
-				console.log(key);
-				delete this.children[key];
+
+	prepend(component, key) {
+		component.parent = this;
+		this.e.children[0].insertBefore(component.e, this.e.children[0].firstChild);
+		this.children[key] = component;
+	}
+
+	replace(component, key) {
+		component.parent = this.parent;
+		this.parent.children[key] = component;
+		this.e.replaceWith(component.e);
+		for(var key in this.parent.children) {
+			if(this.parent.children[key] === this) {
+				delete this.parent.children[key];
 			}
 		}
-		if (this.mounted !== undefined) {
-			this.mounted();
-		}
-	}
-	this.append = function(component, key) {
-		this.cache(component, key);
-		this.anchor.children[0].appendChild(component.anchor);
-		component.mount();
-	}
-	this.prepend = function(component) {
-		this.anchor.children[0].insertBefore(component.anchor, this.anchor.children[0].firstChild);
-		component.mount();
-	}
-	this.replace = function(component) {
-		this.anchor.replaceWith(component.anchor);
-		component.mount();
-	}
-	this.remove = function() {
-		this.anchor.parentNode.removeChild(this.anchor);
 	}
 
-	this.on = function(event, func) {
+	remove(component) {
+		this.e.parentNode.removeChild(this.e);
+	}
+
+	setInnerHTML(html) {
+		this.node.innerHTML = html;
+	}
+
+	getInnerHTML() {
+		return this.node.innerHTML;
+	}
+
+	style(props) {
+		var cssAddValue = function(x, key, value) {
+			var val1 = value.slice(0, value.indexOf(' '));
+			var match;
+			if((match = x.style[key].match(new RegExp(`(^${val1}|, ${val1})`, 'g'))) === null) {
+				x.style[key] += (',' + value);
+			} else {
+				var start = x.style[key].indexOf(match[0]);
+				(match[0].indexOf(',') > -1) ? start += 2 : '';
+				var before = x.style[key].slice(0, start);
+				var middle = value;
+				var end = "";
+				var after = x.style[key].slice(start).indexOf(',');
+				if(after !== -1) {
+					end = x.style[key].slice(x.style[key].slice(start).indexOf(',')); 
+				}
+				x.style[key] = before + middle + end;
+			}
+		}
+
+		var cssObject = function(props, x) {
+			for (var key in props) {
+				if (props.hasOwnProperty(key)) {
+					if(key.indexOf('-') !== -1) {
+						var val = props[key];
+						var idx = key.indexOf('-') + 1;
+						key = key.slice(0, idx - 1) + key[idx].toUpperCase() + key.slice(idx+1);
+						x.style[key] = val;
+					} else {
+						if(key === 'transition' || key === 'animation') {
+							if (props[key] === '' || x.style[key] === '') {
+								x.style[key] = props[key];
+							} else {
+								var stack = props[key];
+								var value = "";
+								while(stack.indexOf(',') !== -1) {
+									value = stack.slice(0, stack.indexOf(',')).trim();
+									cssAddValue(x, key, value);
+									stack = stack.slice(stack.indexOf(',') + 1).trim();
+								}
+								cssAddValue(x, key, stack);
+							}
+						} else {
+							x.style[key] = props[key];
+						}
+					}
+				}
+			}
+		}
+		if (typeof(props) === 'object') {
+			cssObject(props, this.node);
+		} else {
+			if(props.indexOf('-') !== -1) {
+				var idx = props.indexOf('-') + 1;
+				props = props.slice(0, idx - 1) + props.slice(idx, idx + 1).toUpperCase() + props.slice(idx+1);
+			}
+			return this.node.style[props];
+		}
+	}
+
+	on(event, func) {
 		var addListener = function(event, func) {
-			this.anchor.children[0].addEventListener(event, func);
+			this.e.children[0].addEventListener(event, func);
 		}.bind(this)
 
 		if (event.indexOf(' ') !== -1) {
@@ -78,9 +122,9 @@ Zam.component = function() {
 		}
 	}
 
-	this.off = function(event, func) {
+	off(event, func) {
 		var removeListener = function(event, func) {
-			this.anchor.children[0].removeEventListener(event, func);
+			this.e.children[0].removeEventListener(event, func);
 		}.bind(this)
 
 		if (event.indexOf(' ') !== -1) {
