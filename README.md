9ac1f0757518e6db408aab61314bbec5779bfa60
Brandon Barber <roecrew@gmail.com>
2018-01-03 23:20:57 -0600

    5.2

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ http://zamjs.com
 
 ## Overview/Philosophy
 
-Encourages single-page applications.
+A component based micro-library that encourages single-page applications.
 
 This library was created with one goal in mind — to stay close to vanilla.
 
@@ -21,83 +21,8 @@ Zam is component based.
 ## Import
 
 ```html
-<script src="https://cdn.jsdelivr.net/gh/roecrew/zam@5.1/zam.min.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/roecrew/zam@5.2/zam.min.js"></script>
 ```
 ```
 npm install zamjs
-```
-
-## Router Example
-
-For more examples visit http://zamjs.com/examples
-
-### Router - Frontend
-
-```html
-<html>
-  <head>
-    <style>
-      .section {
-        position: absolute;
-        left: 0;
-        top: 60px;
-        width:100%
-        height: calc(100vh - 60px);
-      }
-      .bar-tab {
-        width: 100%;
-        height: 60px;
-        display: flex;
-        jusitify-content: center;
-        align-items: center;
-      }
-    </style>
-  </head>
-  <body>
-  <div class="bar-tab">
-    <div id="home-tab">
-      HOME
-    </div>
-    <div id="stuff-tab">
-      STUFF
-    </div>
-    <div id="about-tab">
-      ABOUT
-    </div>
-  </div>
-  <div class="section" id="home">
-    ... some content ...
-  </div>
-  <div class="section" id="stuff">
-    ... some content ...
-  </div>
-  <div class="section" id="about">
-    ... some content ...
-  </div>
-<script type="module">
-import Zam from "https://cdn.jsdelivr.net/gh/roecrew/zam@5.1/zam.min.js";
-new Zam.router({
-    'home-tab': {view: '#home', 'display': 'flex'}, //the first route will always be the root route. i.e www.somesite.com/
-    'stuff-tab': {view: '#stuff', 'display': 'flex'},
-    'about-tab': {view: '#about', 'display': 'flex'}
-})
-</script>
-</body>
-</html>
-```
-
-### Router - Backend
-
-```javascript
-const http = require("http");
-const fs = require("fs");
-const express = require("express");
-const app = express();
-
-app.get("/*", function(req, res) {
-  res.sendFile(__dirname + "/dist/home.html");
-});
-
-const httpServer = http.createServer(app);
-httpServer.listen(80);
 ```
--- zam.js
+++ zam.js
@@ -2,8 +2,6 @@ export default function Zam() {}
 
 Zam.idCounter = 0;
 
-Zam.routes = {};
-
 Zam.getId = function() {
 	return Zam.idCounter++;
 }
@@ -58,111 +56,39 @@ Zam.component = function() {
 	this.remove = function() {
 		this.anchor.parentNode.removeChild(this.anchor);
 	}
-}
 
-Zam.on = function(event, e, func) {
-	var bind = function(event, e, func) {
-		if(typeof(e) !== 'object') {
-			var x = document.querySelectorAll(e);
-			var len = x.length;
-			for(var i=0;i<len; i++) {
-				x[i].addEventListener(event, func);
+	this.on = function(event, func) {
+		var addListener = function(event, func) {
+			this.anchor.children[0].addEventListener(event, func);
+		}.bind(this)
+
+		if (event.indexOf(' ') !== -1) {
+			var eve = event;
+			while(eve.indexOf(' ') !== -1) {
+				addListener(eve.slice(0, eve.indexOf(' ')), func);
+				eve = eve.slice(eve.indexOf(' ') + 1);
 			}
+			addListener(eve, func);
 		} else {
-			e.addEventListener(event, func);
+			addListener(event, func);
 		}
 	}
 
-	if (e instanceof Zam.component) {
-		e = e.anchor.children[0];
-	}
+	this.off = function(event, func) {
+		var removeListener = function(event, func) {
+			this.anchor.children[0].removeEventListener(event, func);
+		}.bind(this)
 
-	if (event.indexOf(' ') !== -1) {
-		var eve = event;
-		while(eve.indexOf(' ') !== -1) {
-			bind(eve.slice(0, eve.indexOf(' ')), e, func);
-			eve = eve.slice(eve.indexOf(' ') + 1);
-		}
-		bind(eve, e, func);
-	} else {
-		bind(event, e, func);
-	}
-}
-
-Zam.off = function(event, e, func) {
-	var unbind = function(event, e, func) {
-		if(typeof(e) !== 'object') {
-			var x = document.querySelectorAll(e);
-			var len = x.length;
-			for(var i=0;i<len; i++) {
-				x[i].removeEventListener(event, func);
+		if (event.indexOf(' ') !== -1) {
+			var eve = event;
+			while(eve.indexOf(' ') !== -1) {
+				removeListener(eve.slice(0, eve.indexOf(' ')), func);
+				eve = eve.slice(eve.indexOf(' ') + 1);
 			}
+			removeListener(eve, func);
 		} else {
-			e.removeEventListener(event, func);
+			removeListener(event, func);
 		}
 	}
 
-	if (e instanceof Zam.component) {
-		e = e.anchor.children[0];
-	}
-
-	if (event.indexOf(' ') !== -1) {
-		var eve = event;
-		while(eve.indexOf(' ') !== -1) {
-			unbind(eve.slice(0, eve.indexOf(' ')), e, func);
-			eve = eve.slice(eve.indexOf(' ') + 1);
-		}
-		unbind(eve, e, func);
-	} else {
-		unbind(event, e, func);
-	}
-}
-
-Zam.router = function(routes) {
-
-	this.routes = {};
-
-	var updateURL = function(path) {
-		if (history.pushState) {
-			window.history.pushState({path:path},'', path);
-		}
-	}
-
-	var setContent = function() {
-		if(window.location.href.lastIndexOf('/') !== window.location.href.length - 1) {
-			for(var key in this.routes) {
-				if(window.location.href.indexOf('/' + this.routes[key].view.slice(1)) !== -1) {
-					Zam.css({'display': this.routes[key].display}, this.routes[key].view);
-				} else {
-					Zam.css({'display': 'none'}, this.routes[key].view);
-				}
-			}
-		} else {
-			for(var key in this.routes) {
-				Zam.css({'display': this.routes[key].display}, this.routes[key].view);
-				break;
-			}
-		}
-	}.bind(this);
-
-	this.routes = JSON.parse(JSON.stringify(routes));
-
-	setContent();
-
-	Zam.on('popstate', window, () => {
-		setContent();
-	});
-
-	var ids = '';
-	for(var key in this.routes) {
-		var event = 'click';
-		if(key.event !== undefined) {
-			event = key.event;
-		}
-
-		Zam.on(event, '#' + key, (e) => {
-			updateURL('/' + this.routes[e.target.id].view.slice(1));
-			setContent();
-		});
-	}
 }
